d9c669bc2f6cc867b615f4472666303f06f45ed2
alejandro2309 <alejandrovalenci18201495gmail.com>
2019-10-21 22:35:16 -0430

    se agrego el cors putos :V

--- backend/src/java/API/service/ApplicationConfig.java
+++ backend/src/java/API/service/ApplicationConfig.java
@@ -29,6 +29,7 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(API.corsFilter.class);
         resources.add(API.service.DecanatoFacadeREST.class);
         resources.add(API.service.DepartamentoFacadeREST.class);
         resources.add(API.service.EquipoFacadeREST.class);
