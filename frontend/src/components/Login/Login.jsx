3d579c99d33d954a96f1a360dad0c43d072d59e8
Ruben Gutierrez <rubdeivis@gmail.com>
2019-10-22 23:43:45 -0300

    login funciona, agregadp modal

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,46 +1,91 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useState } from "react";
+import axios from "axios";
+
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+
+
+import Modal from '../Modal';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    height: '100vh',
+    height: "100vh"
   },
   image: {
-    backgroundImage: 'url(https://source.unsplash.com/random)',
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
+    backgroundImage: "url(https://source.unsplash.com/random)",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center"
   },
   paper: {
     margin: theme.spacing(8, 4),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+    backgroundColor: theme.palette.secondary.main
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
   },
   submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
+    margin: theme.spacing(3, 0, 2)
+  }
 }));
 
-export default function SignInSide() {
+// const Transition = React.forwardRef(function Transition(props, ref) {
+//   return <Slide direction="up" ref={ref} {...props} />;
+// });
+
+export default function SignInSide(props) {
   const classes = useStyles();
+
+  const [values, setValues] = useState({
+    cedula: "",
+    contrasenna: "",
+    open: false
+  });
+
+  const handleChange = name => event => {
+    setValues({ ...values, [name]: event.target.value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setValues({...values, open:false});
+    axios
+      .post(
+        `http://localhost:8080/backend-lab2/webresources/api.usuario/login/${values.cedula}/${values.contrasenna}`
+      )
+      .then(
+        res => {
+          console.log(res);
+          localStorage.setItem('token', res.data.token);
+          localStorage.setItem('tipoUsuario', res.data.tipo);
+          localStorage.setItem('cedula', res.data.cedula);
+          localStorage.setItem('dpto', res.data.depto);
+          localStorage.setItem('nombre', res.data.depto);
+          console.log(localStorage.getItem('token'));
+          //window.location.href = "/Menu";
+        },
+        error => {
+          console.log(error);
+          setValues({cedula:'',contrasenna:'', open: true });
+        }
+      );
+  };
+
 
   return (
     <Grid container component="main" className={classes.root}>
@@ -54,7 +99,7 @@ export default function SignInSide() {
           <Typography component="h1" variant="h5">
             Inicio de Sesión
           </Typography>
-          <form className={classes.form} noValidate>
+          <form className={classes.form} noValidate onSubmit={handleSubmit}>
             <TextField
               variant="outlined"
               margin="normal"
@@ -65,6 +110,8 @@ export default function SignInSide() {
               name="cedula"
               autoComplete="cedula"
               autoFocus
+              value={values.cedula}
+              onChange={handleChange("cedula")}
             />
             <TextField
               variant="outlined"
@@ -72,13 +119,14 @@ export default function SignInSide() {
               required
               fullWidth
               name="contrasenna"
-              label="contrasenna"
-              type="contrasenna"
+              label="contraseña"
+              type="password"
               id="contrasenna"
               autoComplete="current-contrasenna"
+              value={values.contrasenna}
+              onChange={handleChange("contrasenna")}
             />
 
-            <Link to={'/Menu'}>
             <Button
               type="submit"
               fullWidth
@@ -86,9 +134,29 @@ export default function SignInSide() {
               color="primary"
               className={classes.submit}
             >
-                Iniciar Sesión
+              Iniciar Sesión
             </Button>
-            </Link>
+
+
+            {values.open ? <Modal /> : ''}
+
+            {/* <Dialog
+              open={values.open}
+              TransitionComponent={Transition}
+              keepMounted
+              onClose={handleClose}
+              aria-labelledby="alert-dialog-slide-title"
+              aria-describedby="alert-dialog-slide-description"
+            >
+              <DialogTitle id="alert-dialog-slide-title">
+                {"Error al iniciar sesion"}
+              </DialogTitle>
+              <DialogContent>
+                <DialogContentText id="alert-dialog-slide-description">
+                  Cedula o contraseña inválida
+                </DialogContentText>
+              </DialogContent>
+            </Dialog> */}
 
             <Grid container>
               <Grid item xs>
---
+++ frontend/src/components/Modal.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function Modal() {
+  const [open, setOpen] = React.useState(true);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-slide-title"
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle id="alert-dialog-slide-title">{"Error al iniciar Sesión"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            La cedula o la contraseña no son validas, por favor verique los datos y vuelva a intentar
+          </DialogContentText>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
