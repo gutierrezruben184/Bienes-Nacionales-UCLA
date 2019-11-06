d17e1cd3028e77c4be62296be3104f42486a83e4
Ruben Gutierrez <rubdeivis@gmail.com>
2019-11-05 21:55:32 -0300

    crud equipo listo para generar solicitud

--- frontend/src/components/Equipo.jsx
+++ frontend/src/components/Equipo.jsx
@@ -1,93 +1,141 @@
 import React, { useState, useEffect } from 'react';
 import MaterialTable from 'material-table';
-import axios from "axios";
+import API from "../utils/API";
+import Swal from 'sweetalert2';
+import AssignmentIcon from '@material-ui/icons/Assignment';
 
-export default function MaterialTableDemo() {
-  const [state, setState] = React.useState([]);
+
+const lookupMarcas = (marcas) => {
+  let lookup = {}
+  marcas.map((marca,i) => 
+    (
+  lookup[marca.idmarca] = marca.nombre
+  ))
+  // console.log(lookup)
+  return lookup
+}
+
+export default function Equipo() {
+  const [equipos, setEquipos] = useState([]);
+  const [marcas, setMarcas] = useState([]);
+
 
   async function getEquipos(){
-    try{
-      const response = await axios({
-        url: `http://localhost:8080/UCLA-BN/webresources/api.equipo`,
-        method: 'GET'
+    await API.get("/api.equipo")
+      .then(
+      res => {
+        // console.log(res.data)
+        setEquipos(res.data)
+        // lookupMarcas(res.data.fkIdmarca)
       })
-      return response.data
-  
-    } catch(e){
-      console.log(e)
-  
-    }
+      .catch(e => {
+        console.log("error" + e);
+      })
   }
 
+  async function getMarcas(){
+    await API.get("/api.marca")
+      .then(
+      res => {
+        console.log(res.data)
+        setMarcas(res.data)
+        // lookupMarcas(res.data)
+      })
+      .catch(e => {
+        console.log("error" + e);
+      })
+  }
+
+  
   useEffect(() => {
-    async function loadEquipos () {
-      const response = await getEquipos()
-      console.log(response)
-      setState(response)
-        
-    }
-    
-    loadEquipos()
+    getEquipos()
+    getMarcas()
   }, []);
 
   async function refresh() {
-    const response = await getEquipos()
-      setState(response)
+    await getEquipos()
   }
-
-  async function addEquipo(datos){
-    try{
-      const response = await axios({
-        url: `http://localhost:8080/UCLA-BN/webresources/api.equipo`,
-        method: 'POST',
-        data: {
-                idequipo: datos.idequipo,
-                nombre: datos.nombre,   
-                //fkIdmarca.idmarca: datos.fkIdmarca.idmarca,
-                //fkIddepartamento: datos.fkIddepartamento.idunidad,
-                //fkIdestadoequipo: datos.fkIdestadoequipo.idestadoequipo,
-
-              }
-      })
-      refresh();
-      return response.data
-      } catch(e){
-        console.log(e)
+ 
+  async function addEquipo(newData){
+    await API.post("/api.equipo",
+    {
+        fkIddepartamento: JSON.parse(localStorage.getItem('dpto')),
+        fkIdestadoequipo: {
+          idestadoequipo: newData.fkIdestadoequipo.idestadoequipo,
+          tipo: newData.fkIdestadoequipo.idestadoequipo == "1" ? "activo" : "inactivo" //cambiar
+        }, 
+        fkIdmarca:  marcas.find( marca => marca.idmarca == newData.fkIdmarca.idmarca ),
+        nombre: newData.nombre,
     }
+    )
+    .then(res => {
+        Swal.fire(
+        'Listo!',
+        'Equipo Agregado con Exito!',
+        'success'
+      )
+        refresh()
+      })
+      .catch(error => {
+        console.log(error)
+        Swal.fire({
+          type: 'error',
+          title: 'Oops...',
+          text: 'Algo salió mal!',
+        })
+      })
   }
 
   async function updateEquipos(newData, oldData){
-    try{
-      const response = await axios({
-        url: `http://localhost:8080/UCLA-BN/webresources/api.equipo/`+oldData.idequipo,
-        method: 'PUT',
-        data: {
-                nombre: newData.nombre,
-              }
-      })
-      refresh();
-      return response.data
-      } catch(e){
-        console.log(e)
+    await API.put("/api.equipo/"+newData.idequipo,
+    {
+        fkIddepartamento: newData.fkIddepartamento,
+        fkIdestadoequipo: {
+          idestadoequipo: newData.fkIdestadoequipo.idestadoequipo,
+          tipo: newData.fkIdestadoequipo.idestadoequipo == "1" ? "activo" : "inactivo" //cambiar
+        }, 
+        fkIdmarca: marcas.find( marca => marca.idmarca == newData.fkIdmarca.idmarca ),
+        nombre: newData.nombre,
+        idequipo: newData.idequipo
     }
+    )
+    .then(res => {
+        Swal.fire(
+        'Listo!',
+        'Equipo modificado con Exito!',
+        'success'
+      )
+      })
+      .catch(error => {
+        console.log(error)
+        Swal.fire({
+          type: 'error',
+          title: 'Oops...',
+          text: 'Algo salió mal!',
+        })
+      })
   }
 
   async function DeleteEquipos(idequipo){
-    try{
-      const response = await axios({
-        url: `http://localhost:8080/UCLA-BN/webresources/api.equipo/${idequipo}`,
-        method: 'DELETE'
+    // no se deberia eliminar
+   await API.delete("/api.equipo/"+idequipo,
+    )
+    .then(res => {
+        Swal.fire(
+        'Listo!',
+        'Equipo Eliminado con Exito!',
+        'success'
+      )
+        refresh()
       })
-      let resultado = state.filter( state => (
-        idequipo != idequipo
-   ));
-        refresh();
-        setState(resultado)
-
-    } catch(e){
-      console.log(e)
-
-    }
+      .catch(error => {
+        console.log(error)
+        Swal.fire({
+          type: 'error',
+          title: 'Oops...',
+          text: 'Algo salió mal!',
+        })
+      })
     
   }
   
@@ -95,13 +143,23 @@ export default function MaterialTableDemo() {
   return (
     <MaterialTable
       title="Lista de Equipos"
+      localization = {{
+        body: {
+            editRow: {
+              deleteText: '¿Seguro que quieres borrar?'
+          }
+        }
+      }}
       columns={[
-        { title: 'ID Equipo', field: 'idequipo' },
         { title: 'Nombre', field: 'nombre' },
-        { title: 'ID Marca', field: 'fkIdmarca.idmarca' },
-        { title: 'ID Departamento', field: 'fkIddepartamento.idunidad' },
-        { title: 'ID Estado-Equipo', field: 'fkIdestadoequipo.idestadoequipo'}]}
-      data={state}
+        { title: 'Marca', field: 'fkIdmarca.idmarca', lookup: lookupMarcas(marcas) },
+        { title: 'Estado del Equipo', field: 'fkIdestadoequipo.idestadoequipo', 
+        lookup: {1: 'activo', 2:'no activo'}}
+        ]}
+      data={[
+        {nombre:'prueba'},
+        {nombre:'prueba2'},
+      ]}
       editable={{
         onRowAdd: newData =>
           new Promise(resolve => {
@@ -125,6 +183,13 @@ export default function MaterialTableDemo() {
             }, 600);
           }),
       }}
+      actions={[
+        {
+          icon: () => <AssignmentIcon/>,
+          tooltip: 'Generar reporte',
+          onClick: (event, rowData)  => alert("Dialog para el formulario " + rowData.name)
+        }
+      ]}
     />
   );
 }
